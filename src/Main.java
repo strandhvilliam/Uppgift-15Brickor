9bb710cff9693565d76c4c7c7f519cd47f0735b6
Marcel <mghostine@icloud.com>
2022-10-28 10:47:01 +0200

    Fixed main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
-        //hej hej eh je
 
-        System.out.println("Hello World!");
-        //testcomment
+  public Main(){
+
+
+  }
+
+
+    public static void main(String[] args) {
+      Main m = new Main();
     }
 
 }
